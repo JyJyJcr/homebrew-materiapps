aa9ef9b86c5bbf6ce14cf2e4afce044d2a3bfe58
JyJyJcr <82190170+JyJyJcr@users.noreply.github.com>
2025-04-28 19:50:39 +0900

    feat: conditional compile: use libomp on mac

--- Formula/itensor.rb
+++ Formula/itensor.rb
@@ -8,8 +8,22 @@ class Itensor < Formula
   
     depends_on "cmake" => :build
     depends_on "ninja" => :build
-    #depends_on "libomp"
-    #depends_on "openblas"
+
+    on_macos do
+        depends_on "libomp"
+        fails_with :gcc do
+            cause "libomp is not compatible with gcc"
+        end
+    end
+
+    # not tested yet
+    on_linux do
+        depends_on "openblas"
+        depends_on "gcc" # for libgomp, since openblas is built with this
+        fails_with :clang do
+            cause "openblas use libgomp (in gcc), which is not compatible with clang"
+        end
+    end
   
     def install
         system "cmake", "-GNinja", "-S", ".", "-B", "build",
